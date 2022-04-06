b9c453a8de59a4ed98b805ad5ea34ed353024c00
Mirae Parker <miraep8@gmail.com>
2022-04-06 14:21:45 -0400

    Updating Documentation
    Adding instructions for new contributors.  Adding necessary documents for making this a pip package

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""rendseq."""
-
-__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rendseq',
+      version='0.1.0',
+      description='Package for RendSeq Data Analysis',
+      url='https://github.com/miraep8/rendseq',
+      author='Mirae Parker',
+      author_email='rendseq@mit.edu',
+      license='MIT',
+      packages=['rendseq'],
+      zip_safe=False)
